d408664cecebd8b3c789d978a59a354e24488fcc
Komainu <123217366+Komainu@users.noreply.github.com>
2026-01-21 19:10:50 +0900

    Simplify content in page component

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,7 @@ export default function Page() {
   return (
     <main style={{ padding: 24 }}>
       <h1>Narrative Scanner</h1>
-
-      <p>
-        This mini app is live.
-      </p>
-
-      <p>
-        Bullish / Neutral / Bearish narratives will be displayed here.
-      </p>
-
-      <hr />
-
-      <small>
-        Powered by Next.js on Vercel
-      </small>
+      <p>Mini App is alive.</p>
     </main>
-  )
+  );
 }
